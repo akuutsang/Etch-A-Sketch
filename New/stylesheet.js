ef1c367d04280f23720c42fc8dd11bbd3a5bc4d9
Akutsang <mymac@MYs-MacBook-Pro.local>
2023-01-19 13:47:05 +0100

    new function chnageSize to be able to change the size of the grid board

--- New/stylesheet.js
+++ New/stylesheet.js
@@ -1,1 +1,15 @@
 let board = document.querySelector(".grid-container");
+
+function styleBoard(){
+    for (let i = 0; i <256; i++) {
+        let square = document.createElement("divs")
+        square.style.backgroundColor ="blue"
+        board.insertAdjacentElement("beforeend", square)
+    }
+}
+
+styleBoard();
+
+function changeSize(input){
+ styleBoard(input)
+}
