e51575c2a26216deed76f66f5a48e2bad8f0af4d
Akutsang <mymac@MYs-MacBook-Pro.local>
2023-01-19 14:45:56 +0100

    event listener for square

--- New/stylesheet.js
+++ New/stylesheet.js
@@ -2,17 +2,23 @@ function styleBoard(size){
     let board = document.querySelector(".board");
     let squares = board.querySelectorAll("div");
     squares.forEach((div) => div.remove());
-    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     let amount = size * size;
     for (let i = 0; i < amount; i++) {
         let square = document.createElement("div")
-        square.style.backgroundColor ="blue"
+        square.addEventListener("mouseover", ()=> {
+            square.style.backgroundColor = "black";
+        });
+        square.style.backgroundColor = "blue"
         board.insertAdjacentElement("beforeend", square)
     }
 }
 styleBoard(16);
 
 function changeSize(input){
- styleBoard(input)
+    if (input >= 2 || input <= 100){
+        styleBoard(input);
+    } else{
+        console.log("too many squares")
+    }
+   
 }
